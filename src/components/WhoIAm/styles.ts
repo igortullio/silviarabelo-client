5ea6ce8980ff788ed4bce6ee19532eb1ba08ec91
Igor Tullio <igortullio@hotmail.com>
2020-09-13 10:49:01 -0300

    Fix image height

--- src/components/WhoIAm/styles.ts
+++ src/components/WhoIAm/styles.ts
@@ -4,7 +4,7 @@ import media from 'styled-media-query';
 export const Image = styled.img`
   display: block;
   max-width: 100%;
-  max-height: min(30rem, 100%);
+  max-height: 30rem;
   margin: 0 auto;
 
   ${media.greaterThan('medium')`
